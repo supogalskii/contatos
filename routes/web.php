9827fc3877b4d1d90253e7c3973576c62ef4e735
SUELEN POGALSKI <suelen.pogalski@unidavi.edu.br>
2022-10-05 17:10:57 -0300

    add contatos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContatosController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('contatos',ContatosController::class);
